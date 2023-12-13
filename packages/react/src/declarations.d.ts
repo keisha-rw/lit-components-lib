8d20e97b29bdb9aff478bb3440f9a1b1df1a90d8
Keisha Hoeksema <49070129+keisha-rw@users.noreply.github.com>
2023-12-13 15:58:23 -0600

    Add files via upload

---
+++ packages/react/src/declarations.d.ts
@@ -0,0 +1,5 @@
+// declarations.d.ts
+declare module '@brightcove/react-player-loader';
+declare module '@keisha/design-system/utils/video-player/video-player-utils';
+declare module 'uuid';
+declare module '@lit-labs/ssr-react/jsx-runtime';
