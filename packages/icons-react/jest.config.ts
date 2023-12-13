53bc9158978894f825358e382cfdc7597703990b
Keisha Hoeksema <49070129+keisha-rw@users.noreply.github.com>
2023-12-13 14:22:31 -0600

    Add files via upload

---
+++ packages/icons-react/jest.config.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+export default {
+  displayName: 'icons-react',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/icons-react',
+};
